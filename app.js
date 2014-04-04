eb1cf6712c6b7c311a3bf2cd9c1e8475804a1a81
Ike Gandhi <ikegandhi@gmail.com>
2014-04-03 20:08:31 -0400

    l

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function App(){
   var self = this;
   this.complete = 0;
-      window.matchesArray;
+      window.matchesArray = "aaa";
 
   this.pdfToText = function(data){    
     var div = document.getElementById('viewer');
@@ -27,16 +27,13 @@ function App(){
       context.fillStyle = 'rgb(255, 255, 255)';
       context.fillRect(0, 0, canvas.width, canvas.height);
       context.restore();
-      
-      self.setMessage("Rendering...");
-      
+            
       var textLayer = document.createElement('div');
       textLayer.className = 'textLayer';
       document.body.appendChild(textLayer);
       
       page.startRendering(context, function(){
         if (++self.complete == total){
-          self.setMessage("Finished rendering. Extracting text...");
           
           window.setTimeout(function(){
             var layers = [];
@@ -65,7 +62,6 @@ function App(){
   this.receiveInput = function(event){
     if (event.source != parent) return;
     if (!event.data.byteLength) return alert("The PDF data needs to be an ArrayBuffer");
-    self.setMessage("Received data");
     self.pdfToText(event.data);
   }
   
@@ -74,11 +70,6 @@ function App(){
     recipient.postMessage(text, "*");
   };
   
-  this.setMessage = function(text){
-    document.getElementById("message").textContent = text;
-  }
-
   window.addEventListener("message", self.receiveInput, true);
-  self.setMessage("Ready");
   self.sendOutput("ready"); 
 }
